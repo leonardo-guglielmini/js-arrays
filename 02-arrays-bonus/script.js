3273c34b3e0051d693c50540191293e4c2ec3ca5
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-10-16 14:03:48 +0200

    ADD exercise 02-array-bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,30 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+//const reversedTeachers = teachers.reverse(); //questa operazione modifica l'ordine dell'array originale
+const reversedTeachers = teachers.slice().reverse(); //questa operazione NON modifica l'ordine dell'array originale
+//console.log(reversedTeachers);
+
+/*Possibile anche il seguente metodo "manuale"
+const reversedTeachers=[];
+for(let i=0; i< teachers.length; i++)
+{
+  reversedTeachers[i]=teachers[teachers.length-i-1];
+}
+console.log(reversedTeachers);
+*/
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for(let i=0;i<teachers.length;i++){
+  if(teachers[i].length>=5){
+    longNames.push(teachers[i]);
+  }
+}
+//console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(teachers.indexOf("Ed"),1);
+console.log(teachers);
